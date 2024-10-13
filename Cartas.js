b9425177f340fd1e8ff6c6206eda1455e46d9f2a
CamiLopez15 <est.mariac.lopezm@unimilitar.edu.co>
2024-10-13 01:40:46 -0500

    Cartas, compartir en whatsapp, falta iconos

--- Cartas.js
+++ Cartas.js
@@ -6,3 +6,10 @@ function downloadImage(imgPath, fileName) {
     a.click();
     document.body.removeChild(a);
 }
+
+function shareOnWhatsApp(imgPath) {
+    var fullURL = "https://camilopez15.github.io/Paginaaa/" + imgPath;
+    var message = "Mira esta carta, te lo manda alguien especial <3: " + fullURL;
+    var whatsappWebURL = "https://web.whatsapp.com/send?text=" + encodeURIComponent(message);
+    window.open(whatsappWebURL, '_blank');
+}

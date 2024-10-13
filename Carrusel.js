1a93e14046bfbadb9c75e20f570d60d9669332cd
CamiLopez15 <est.mariac.lopezm@unimilitar.edu.co>
2024-10-13 03:25:29 -0500

    Pruebas de carrusel

--- Carrusel.js
+++ Carrusel.js
@@ -1,8 +1,50 @@
-let currentSlide = 0;
+let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    
+    let active = 3;
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
 
-function moveSlide(direction) {
-    const slides = document.querySelectorAll('.carousel-slide');
-    currentSlide = (currentSlide + direction + slides.length) % slides.length; // Mueve entre los slides
-    const rotationDegree = currentSlide * -120; // Cada slide gira 120 grados
-    document.querySelector('.carousel').style.transform = `rotateY(${rotationDegree}deg)`; // Aplica la rotación
-}
+        // Pausar todos los audios cuando se cambia de slide
+        pauseAllAudios();
+
+
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+    function pauseAllAudios() {
+        const audios = document.querySelectorAll('audio');
+        audios.forEach(audio => {
+            audio.pause();
+            audio.currentTime = 0;  // Reiniciar el audio al inicio
+        });
+    }
+    
+    
+    loadShow();
+    next.onclick = function(){
+        active = active + 1 < items.length ? active + 1 : active;
+        loadShow();
+    }
+    prev.onclick = function(){
+        active = active - 1 >= 0 ? active - 1 : active;
+        loadShow();
+    }

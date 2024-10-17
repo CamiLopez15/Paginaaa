7e622963848ad8e65ac5f19de88728a71712d86e
CamiLopez15 <est.mariac.lopezm@unimilitar.edu.co>
2024-10-17 01:57:38 -0500

    Reiniciar los audios

--- Versione.js
+++ Versione.js
@@ -1,12 +1,13 @@
 // Seleccionar todos los elementos de audio
 const audios = document.querySelectorAll('.audio-control');
 
-// Función para pausar otros audios cuando uno se reproduce
+// Función para pausar y reiniciar otros audios cuando uno se reproduce
 audios.forEach(audio => {
     audio.addEventListener('play', () => {
         audios.forEach(otherAudio => {
             if (otherAudio !== audio) {
-                otherAudio.pause();  // Pausar cualquier otro audio
+                otherAudio.pause();     // Pausar cualquier otro audio
+                otherAudio.currentTime = 0; // Reiniciar el audio al inicio
             }
         });
     });

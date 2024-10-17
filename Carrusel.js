c2de0c7f857c0262adb013bc49f4f8dd21649cc8
CamiLopez15 <est.mariac.lopezm@unimilitar.edu.co>
2024-10-16 23:52:56 -0500

    Cartas primera evolucion

--- Carrusel.js
+++ Carrusel.js
@@ -48,3 +48,18 @@ let items = document.querySelectorAll('.slider .item');
         active = active - 1 >= 0 ? active - 1 : active;
         loadShow();
     }
+    function downloadImage(imgPath, fileName) {
+        var a = document.createElement('a');
+        a.href = imgPath;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+    
+    function shareOnWhatsApp(imgPath) {
+        var fullURL = "https://camilopez15.github.io/Paginaaa/" + imgPath;
+        var message = "Mira esta carta, te lo manda alguien especial <3: " + fullURL;
+        var whatsappWebURL = "https://web.whatsapp.com/send?text=" + encodeURIComponent(message);
+        window.open(whatsappWebURL, '_blank');
+    }
